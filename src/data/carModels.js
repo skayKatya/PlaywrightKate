7628f0d1d193cf9931b40701c2c4078582a4e504
Kate <skay.katya@gmail.com>
2024-08-22 12:42:36 +0300

    HW28-1

---
+++ src/data/carModels.js
@@ -0,0 +1,135 @@
+import {CAR_BRANDS} from "./cars.js";
+
+export const CAR_MODELS = {
+   AUDI : {
+        TT:   {
+                "id": 1,
+                "carBrandId": 1,
+                "title": "TT"
+            },
+        R8:   {
+                "id": 2,
+                "carBrandId": 1,
+                "title": "R8"
+            },
+        Q7:   {
+                "id": 3,
+                "carBrandId": 1,
+                "title": "Q7"
+            },
+        A6:    {
+                "id": 4,
+                "carBrandId": 1,
+                "title": "A6"
+            },
+        A8:   {
+                "id": 5,
+                "carBrandId": 1,
+                "title": "A8"
+            }
+    }, 
+
+    BMW : {
+        3:   {
+                "id": 6,
+                "carBrandId": 2,
+                "title": "3"
+            },
+        5:   {
+                "id": 7,
+                "carBrandId": 2,
+                "title": "5"
+            },
+        X5:   {
+                "id": 8,
+                "carBrandId": 2,
+                "title": "X5"
+            },
+        X6:    {
+                "id": 9,
+                "carBrandId": 2,
+                "title": "X6"
+            },
+        Z3:   {
+                "id": 10,
+                "carBrandId": 2,
+                "title": "Z3"
+            }
+    },
+
+    FORD : {
+        Fiesta:   {
+                "id": 11,
+                "carBrandId": 3,
+                "title": "Fiesta"
+            },
+        Focus:   {
+                "id": 12,
+                "carBrandId": 3,
+                "title": "Focus"
+            },
+        Fusion:   {
+                "id": 13,
+                "carBrandId": 3,
+                "title": "Fusion"
+            },
+        Mondeo:    {
+                "id": 14,
+                "carBrandId": 3,
+                "title": "Mondeo"
+            },
+        Sierra:   {
+                "id": 15,
+                "carBrandId": 3,
+                "title": "Sierra"
+            }
+    },
+
+    PORSCHE : {
+        911:   {
+                "id": 16,
+                "carBrandId": 4,
+                "title": "911"
+            },
+        Cayenne:   {
+                "id": 17,
+                "carBrandId": 4,
+                "title": "Cayenne"
+            },
+        Panamera:   {
+                "id": 18,
+                "carBrandId": 4,
+                "title": "Panamera"
+            }
+    },
+
+    FIAT : {
+        Palio:   {
+                "id": 19,
+                "carBrandId": 5,
+                "title": "Palio"
+            },
+        Ducato:   {
+                "id": 20,
+                "carBrandId": 5,
+                "title": "Ducato"
+            },
+        Panda:   {
+                "id": 21,
+                "carBrandId": 5,
+                "title": "Panda"
+            },
+        Punto:    {
+                "id": 22,
+                "carBrandId": 5,
+                "title": "Punto"
+            },
+        Scudo:   {
+                "id": 23,
+                "carBrandId": 5,
+                "title": "Scudo"
+            }
+    }
+
+
+}
